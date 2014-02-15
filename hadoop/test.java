b3fec16c53a41ed63ce48e514ed247aa0dad3e8e
diaoyouwei <thinkingdyw@gmail.com>
2014-02-15 12:52:42 +0800

    Update test.java

--- hadoop/test.java
+++ hadoop/test.java
@@ -1,10 +1,10 @@
 public class MultiOutput {
 
 	public static class MultiOutputMapper extends Mapper<LongWritable, Text, LongWritable, Text>{
-  @Override
-  protected void map(LongWritable key, Text value, Context context)
+  		@Override
+  		protected void map(LongWritable key, Text value, Context context)
 				throws IOException, InterruptedException {
-  //业务代码
+  			//业务代码
 			context.write(key, value);
 		}
 	}
@@ -13,7 +13,7 @@ public class MultiOutput {
 		@Override
 		protected void reduce(LongWritable key, Iterable<Text> value,Context context)
 				throws IOException, InterruptedException {
-   //业务代码
+   			//业务代码
 		}
 	}
 }
