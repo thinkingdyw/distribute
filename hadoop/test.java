68d16b4f875707cc332f0498c231b1f1115c9dbe
diaoyouwei <thinkingdyw@gmail.com>
2014-02-15 12:52:12 +0800

    Update test.java

--- hadoop/test.java
+++ hadoop/test.java
@@ -1,32 +1,19 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Properties;
- 
-public class ExampleDecision {
-    public static void main (String[] args) {
-       String text = args[0];
-        try{
-          File file = new File(System.getProperty("oozie.action.output.properties"));
-          Properties props = new Properties();
- 
-          if (text.equals("yes")) {
-                props.setProperty("key1", "value1");
-                props.setProperty("key2", "value2");
-          } else {
-                props.setProperty("key1", "novalue");
-                props.setProperty("key2", "novalue");
-          }
- 
-          OutputStream os = new FileOutputStream(file);
-          props.store(os, "");
-          os.close();
-          System.out.println(file.getAbsolutePath());
-        }
-        catch (Exception e) {
-          e.printStackTrace();
-        }
-    }
+public class MultiOutput {
+
+	public static class MultiOutputMapper extends Mapper<LongWritable, Text, LongWritable, Text>{
+  @Override
+  protected void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+  //业务代码
+			context.write(key, value);
+		}
+	}
+	public static class MultiOutputReducer extends Reducer<LongWritable, Text, NullWritable, Text>{
+		
+		@Override
+		protected void reduce(LongWritable key, Iterable<Text> value,Context context)
+				throws IOException, InterruptedException {
+   //业务代码
+		}
+	}
 }
