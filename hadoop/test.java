0f981a1c1d5c0c2efa6ca5160c767d8fe0be0510
thinkingdyw <thinkingdyw@gmail.com>
2014-05-25 14:43:14 +0800

    Update test.java

--- hadoop/test.java
+++ hadoop/test.java
@@ -53,4 +53,42 @@ public class WordCount{
 			}
 		}
 	}
+	public List<InputSplit> getSplits(JobContext job
+                                    ) throws IOException {
+               	minSize = Math.max(getFormatMinSplitSize(), getMinSplitSize(job));
+    		long maxSize = getMaxSplitSize(job);
+    		// generate splits
+    		List<InputSplit> splits = new ArrayList<InputSplit>();
+    		for (FileStatus file: listStatus(job)) {
+		        Path path = file.getPath();
+		        FileSystem fs = path.getFileSystem(job.getConfiguration());
+		        long length = file.getLen();
+			BlockLocation[] blkLocations = fs.getFileBlockLocations(file, 0, length);
+			if ((length != 0) && isSplitable(job, path)) { 
+			        long blockSize = file.getBlockSize();
+			        long splitSize = computeSplitSize(blockSize, minSize, maxSize);
+
+        			long bytesRemaining = length;
+        			while (((double) bytesRemaining)/splitSize > SPLIT_SLOP) {
+          				int blkIndex = getBlockIndex(blkLocations, length-bytesRemaining);
+          				splits.add(new FileSplit(path, length-bytesRemaining, splitSize, 
+                                   	blkLocations[blkIndex].getHosts()));
+          				bytesRemaining -= splitSize;
+        			}
+        
+        			if (bytesRemaining != 0) {
+          				splits.add(new FileSplit(path, length-bytesRemaining, bytesRemaining, 
+                     			blkLocations[blkLocations.length-1].getHosts()));
+        		}		
+	      		} else if (length != 0) {
+	        		splits.add(new FileSplit(path, 0, length, blkLocations[0].getHosts()));
+	      		} else { 
+        			//Create empty hosts array for zero length files
+        			splits.add(new FileSplit(path, 0, length, new String[0]));
+      			}
+    		}
+    		LOG.debug("Total # of splits: " + splits.size());
+    		return splits;
+  	}
+
 }
