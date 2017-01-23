83d0acc09c365793668f9c5ea1be12608e9a0e1e
diaoyouwei <diaoyouwei@didichuxing.com>
2017-01-23 08:56:15 +0800

    ..

---
+++ rpc/src/main/java/com/thinking/rpc/App.java
@@ -0,0 +1,13 @@
+package com.thinking.rpc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
